b2cc48a1702b16a810382c27d4366a03b2d33853
linxi <2790673868@qq.com>
2021-07-10 13:32:17 +0800

    init

---
+++ src/main/java/org/sacc/SaccHome/SaccHomeApplication.java
@@ -0,0 +1,13 @@
+package org.sacc.SaccHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaccHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaccHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sacc/SaccHome/SaccHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sacc.SaccHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaccHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
