1553ac9f901d3bfd63278cbd0eedce0af1c3c729
tx2002 <76741680+tx2002@users.noreply.github.com>
2021-07-19 23:45:54 +0800

    预约时间重复的判断

--- src/main/java/org/sacc/SaccHome/mapper/OrderMapper.java
+++ src/main/java/org/sacc/SaccHome/mapper/OrderMapper.java
@@ -20,4 +20,11 @@ public interface OrderMapper {
      */
     int save(Order order);
 
+    /**
+     * 判断时间段a-b是否与已有的预约时间重复
+     * @param order
+     * @return
+     */
+    List<Order> judgeTimeCorrect(Order order);
+
 }
