37b3132cf693bed60335690ec9243306e7145a3c
linxi <2790673868@qq.com>
2021-08-09 20:56:03 +0800

    新增userInfo

---
+++ src/main/java/org/sacc/SaccHome/mbg/model/UserInfo.java
@@ -0,0 +1,23 @@
+package org.sacc.SaccHome.mbg.model;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+    private Integer id;
+
+    private String nick;
+
+    private String department;
+
+    private String realName;
+
+    private String studentId;
+
+    private Integer phoneNumber;
+
+    private String imgUrl;
+
+    private Integer userId;
+
+}
